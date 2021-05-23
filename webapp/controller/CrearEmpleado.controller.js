2d84edb971a9431a92c185ce5a91735264d8ab25
erikaruizp <erikaruizp@gmail.com>
2021-05-23 23:50:30 +0000

    02_CrearEmpleado

--- webapp/controller/CrearEmpleado.controller.js
+++ webapp/controller/CrearEmpleado.controller.js
@@ -17,10 +17,12 @@ sap.ui.define([
                 _tipoEmpleado : "",
                 _min: 0,
                 _max: 0,   
+                _escala: 0,
                 _nombreState :"None",
                 _apellidoState :"None",
                 _dniState :"None",                                             
                 _fechaState :"None",
+                sapId : this.getOwnerComponent().SapId,
                 tipo: "",
                 nombres: "",
                 apellidos: "",
@@ -41,6 +43,7 @@ sap.ui.define([
             this.getView().setModel(this._model);
 
             this._wizard = this.byId("wizard");
+            this._empleadoID = "";
         };
         function onCancel(){
             var oResource = this.getView().getModel("i18n").getResourceBundle(); 
@@ -60,6 +63,7 @@ sap.ui.define([
             this._model.setProperty("/_tipoEmpleado","Interno");
             this._model.setProperty("/_min",12000);
             this._model.setProperty("/_max",80000);
+            this._model.setProperty("/_escala", 1000);
             this._model.setProperty("/tipo","0");
             this._model.setProperty("/sueldo",24000);                                    
             this._wizard.nextStep();        
@@ -68,6 +72,7 @@ sap.ui.define([
             this._model.setProperty("/_tipoEmpleado","Autonomo");
             this._model.setProperty("/_min",100);
             this._model.setProperty("/_max",2000);
+            this._model.setProperty("/_escala", 50);
             this._model.setProperty("/tipo","1");
             this._model.setProperty("/sueldo",400);                
             this._wizard.nextStep();        
@@ -76,6 +81,7 @@ sap.ui.define([
             this._model.setProperty("/_tipoEmpleado","Gerente");
             this._model.setProperty("/_min",50000);
             this._model.setProperty("/_max",200000);
+            this._model.setProperty("/_escala", 1000);
             this._model.setProperty("/tipo","2");
             this._model.setProperty("/sueldo",70000);              
             this._wizard.nextStep();        
@@ -160,7 +166,7 @@ sap.ui.define([
 
                     if (archivos.length > 0) {
                         for(var i in archivos){
-                            tabla.push({DocName:archivos[i].getFileName(),MimeType:archivos[i].getMimeType()});	
+                            tabla.push({NombreDoc:archivos[i].getFileName(),TipoMime:archivos[i].getMimeType()});	
                         }
                     }
                     //Ir a página Resumen
@@ -172,8 +178,90 @@ sap.ui.define([
                 }
             }.bind(this));
             var oData = this._model.getData();
-        }
-              
+        };
+        function onIrPaso1() {
+            //Ir a sección Tipo Empleado
+            this._model.setProperty("/_layout", "OneColumn");            
+            this._wizard.goToStep(this.byId("typeEmployeeStep"));            
+        };
+        function onIrPaso2() {
+            //Ir a sección Datos Empleado
+            this._model.setProperty("/_layout", "OneColumn");            
+            this._wizard.goToStep(this.byId("dataEmployeeStep"));            
+        };        
+        function onIrPaso3() {
+            //Ir a sección Datos Opcionales
+            this._model.setProperty("/_layout", "OneColumn");            
+            this._wizard.goToStep(this.byId("OptionalInfoStep"));            
+        };  
+        function onFileChange(oEvent) {
+            let oUploadCollection = oEvent.getSource();
+            //Header Token CSRF
+            let parameterToken = new sap.m.UploadCollectionParameter({
+                name: "x-csrf-token",
+                value: this.getView().getModel("employeeModel").getSecurityToken()                
+            });
+            oUploadCollection.addHeaderParameter(parameterToken);            
+        };     
+        function onBeforeUpload (oEvent) {
+            let parameterSlug = new sap.m.UploadCollectionParameter({
+                name: "slug",
+                value: this.getOwnerComponent().SapId + ";" + this._empleadoID + ";" + oEvent.getParameter("fileName")
+            });
+            oEvent.getParameters().addHeaderParameter(parameterSlug);            
+        };         
+        function onGrabar() {
+            var oData = this.getView().getModel().getData();
+            var body = {
+                SapId : oData.sapId,
+                Type : oData.tipo,
+                FirstName : oData.nombres,
+                LastName: oData.apellidos,
+                Dni: oData.dni,
+                CreationDate: oData.fechaIn,
+                Comments: oData.comentarios,
+                UserToSalary: [
+                    {Ammount : parseFloat(oData.sueldo).toString(),
+                     Comments : oData.comentarios,
+                     Waers : "EUR"}
+                    ]
+            };
+            this.getView().setBusy(true);
+            this.getView().getModel("employeeModel").create("/Users",body,{
+                success : function(data){
+                    var oResourceB = this.oView.getModel("i18n").getResourceBundle();
+                    var oMensaje = oResourceB.getText("empleadoNuevoTxt");
+                    this.getView().setBusy(false);
+                    //ID del nuevo usuario
+                    this._empleadoID = data.EmployeeId;
+                    oMensaje = oMensaje + this._empleadoID;
+                    sap.m.MessageBox.information( oMensaje ,{
+                        onClose : function(){
+                            //Ir al wizard
+                            this._model.setProperty("/_layout", "OneColumn");            
+//                            this._wizard.goToStep(this.byId("typeEmployeeStep"));
+                            //Ir al menú principal
+                            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                            oRouter.navTo("RouteMenu",{},true);
+                        }.bind(this)
+                    });
+                    this.onSubirArchivos();
+                }.bind(this),
+                error : function(){
+                    var oResourceB = this.oView.getModel("i18n").getResourceBundle();
+                    var oMensaje = oResourceB.getText("empleadoErrorTxt");                    
+                    sap.m.MessageBox.information(oMensaje);
+                                       
+                    this.getView().setBusy(false);
+                }.bind(this)
+            });            
+        };
+        function onSubirArchivos (ioNum) {
+            var contexto = this;
+            var oUploadCollection = contexto.byId("UploadCollection");
+            oUploadCollection.upload();
+        };  
+
         var Main = Controller.extend("logaligroup.Empleados.controller.CrearEmpleado", { });
         
         Main.prototype.onBeforeRendering = onBeforeRendering;
@@ -185,6 +273,13 @@ sap.ui.define([
         Main.prototype.onValidarEmpleado = onValidarEmpleado;
         Main.prototype.onValidarDNI = onValidarDNI;
         Main.prototype.onWizardCompleted = onWizardCompleted;
+        Main.prototype.onIrPaso1 = onIrPaso1;
+        Main.prototype.onIrPaso2 = onIrPaso2;
+        Main.prototype.onIrPaso3 = onIrPaso3;   
+        Main.prototype.onFileChange = onFileChange;   
+        Main.prototype.onBeforeUpload = onBeforeUpload;
+        Main.prototype.onGrabar = onGrabar;
+        Main.prototype.onSubirArchivos = onSubirArchivos;        
 
 		return Main;
 	});
